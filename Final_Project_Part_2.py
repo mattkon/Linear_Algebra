11b2f11e1402091c7a845d69056d6bc3900b230f
mattkon <matthewkonyndyk@Matthews-MacBook-Pro.local>
2017-03-15 13:05:04 -0700

    Update for part 2. Still not done.

--- Final_Project_Part_2.py
+++ Final_Project_Part_2.py
@@ -2,58 +2,134 @@
 # Math 343 Winter 2017
 # Final Project Part 2
 
-
+### DESCRIPTION OF ASSIGNMENT ######################################################################
 
 # Part 2: Use some of the functions from your library, for an appropriately given input matrix A, and
 # 1. Implement the GMRES algorithm with restart (provided in a separate .pdf file).
 # 2. Perform study when you vary mmax = 5, 10, 15, ... the maximal number of steps allowed in the algorithm, and document the total number of iterations and total time to convergence for a given tolerance  (and a given matrix). Write your conclusions from the study.
 
 
-
+### BACKGROUND INFORMATION ON LEAST SQUARES ########################################################
 
 # Least Squares: if A*x=b has no solution, you can find the least squares approximation using the formula (A^T)*A*(x`)=(A^T)*b
-
 # This will minimize the distance ||b-A*x`|| betwen the least squares solution and the solution we wanted.
+# Note - A*(x`) is the projection of b onto the column space of A.
+# Gram Schmidt: Given a Matrix with arbitray basis, Gram Schmidt creates an orthogonal basis.
+# QR decomposition: Q - Orthogonal Matrix, R - Upper Triangular Matrix; R = (Q^T)*M
+# Using QR to solve least squares: R*x = (Q^T)*b
 
-# A*(x`) is the projection of b onto the column space of A.
+
+### GMRES ALGORITHM: An Iterative Least Squares Algorithm ##########################################
+
+# We start with an initial approximation guess x0 (x0 is arbitray, I begin with zeros.) we also have input tolerance (e) and number of iterations(m_max)
+# The algorithm will stop once ||b-A*x`|| < e, or m_max has been reached
+
 
 import scipy as sp
 import numpy as np
 
 
-def GMRES(A, b, x0, e, m_max):
+def GMRES(A, b):
+    e = .0001
+    m_max = 50
     x = [] # initialize x
     p = [0] * (m_max) #creates list p
-    r = np.asarray(b - A.dot(x0)).reshape(-1) #initial residual r0
-    p[0] = r / np.linalg.norm(r) #1st p vector
-    h = np.zeros((m_max + 1, m_max)) #initialize h vector
+    R = np.zeros((m_max + 1, m_max)) #initialize a 50x51 matrix of zeros
+ 
+ 
+ 
+    x0 = np.zeros(5) #initial approximation
+    r = np.asarray(b - A.dot(x0)).reshape(-1) # r0 = b - A*x0 (intial residual - the distance between guesses and our ideal solution). Because we are starting with a 0 vector, r = b. reshape(-1) reshapes the result as a vector (Numpy thing)
+    p[0] = r / np.linalg.norm(r) #p1 = r divided by r_norm  (p1 starts at p[0]) Orthogonalizes?
+    
+    
     
     for k in range(m_max):
         y = np.asarray(A.dot(p[k])).reshape(-1)
         for j in range(k):
             h[j, k] = np.dot(p[j], y)
             y = y - h[j, k] * p[j]
-        h[k + 1, k] = np.linalg.norm(y)
-        if (h[k + 1, k] != 0 and k != m_max - 1):
-            p[k + 1] = y / h[k + 1, k]
+        R[k + 1, k] = np.linalg.norm(y)
+        if (R[k + 1, k] != 0 and k != m_max - 1):
+            p[k + 1] = y / R[k + 1, k]
     
         b = np.zeros(m_max + 1)
         b[0] = np.linalg.norm(r)
         
-        result = np.linalg.lstsq(h, b)[0]
         
+        
+        result = np.linalg.lstsq(R, b)[0]
+
+
+
     x.append(np.dot(np.asarray(p).transpose(), result) + x0)
 
     return x
 
 
 
+
+
+def GMRes(A, b, x0, e, nmax_iter, restart=None):
+    r = b - np.asarray(sp.dot(A, x0)).reshape(-1)
+    
+    x = []
+    q = [0] * (nmax_iter)
+    
+    x.append(r)
+    
+    q[0] = r / np.linalg.norm(r)
+    
+    h = np.zeros((nmax_iter + 1, nmax_iter))
+    
+    for k in range(nmax_iter):
+        y = np.asarray(sp.dot(A, q[k])).reshape(-1)
+        
+        for j in range(k):
+            h[j, k] = np.dot(q[j], y)
+            y = y - h[j, k] * q[j]
+        h[k + 1, k] = np.linalg.norm(y)
+        if (h[k + 1, k] != 0 and k != nmax_iter - 1):
+            q[k + 1] = y / h[k + 1, k]
+    
+        b = np.zeros(nmax_iter + 1)
+        b[0] = np.linalg.norm(r)
+        
+        result = np.linalg.lstsq(h, b)[0]
+        
+    x.append(np.dot(np.asarray(q).transpose(), result) + x0)
+
+return x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 A = np.random.random((5, 5)) #creates random 5x5 matrix
 b = np.random.random(5) #creates random solution vector
-x0 = np.random.random(5) #initial approximation
-e = 10**-6 #input tolerance
-m_max = 50 #Upper bound for number of iteration steps
-result = GMRES(A, b, x0, e, m_max)
+result = GMRES(A, b)
 print(result)
 
 
